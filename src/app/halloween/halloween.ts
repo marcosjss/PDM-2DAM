128b88d4685d64f92635d450b9c69551d4340fe8
marcosjss <marcosjss1234@gmail.com>
2025-10-19 00:07:53 +0200

    Add files via upload

--- src/app/halloween/halloween.ts
+++ src/app/halloween/halloween.ts
@@ -22,31 +22,67 @@ export class Halloween {
       nombre: ['', [Validators.required, Validators.minLength(3)]],
       email: ['',[Validators.required, Validators.email]],
       tipoInvitado:['',[Validators.required]],
-      disfraz:['',[Validators.required]],
+      disfraz:[[Validators.required]],
       fechaLlegada:['',[Validators.required]],
       aceptaReglas: [false, [Validators.requiredTrue]]
 
     })
   }
 
+  //Submit
+
+  formEnviado = false;
+  mensaje: String ="";
+
   comprobar(){
-    if(this.fiesta.invalid) {
-      console.log("El formulario contiene errores");
-      
-    } else {
-      console.log(this.fiesta.value);
-    }
+    this.formEnviado = true;
+    console.log(this.fiesta.value);
+
+    if (this.fiesta.invalid) {
+      console.log('El formulario contiene errores');
+      Object.values(this.fiesta.controls).forEach(control => {control.markAsTouched();});
+      this.mensaje = "";
+      return;
+  }
+
+  const nombre = this.fiesta.get('nombre')?.value;
+  this.mensaje =`🎃 ¡Bienvenido/a, ${nombre}! Tu entrada ha sido registrada con exito`;
+
+  this.formEnviado = false;
+  //this.resetear();
   }
 
   resetear() {
-    this.fiesta.reset({
-      nombre: '',
-      email: '',
-      tipoInvitado:'',
-      disfraz:'',
-      fechaLlegada:'',
-      aceptaReglas:''
-    });
+    this.fiesta.reset();
+    this.mensaje = ""
   }
-}
 
+
+  //Cuenta atras Halloween
+
+  mensajeHalloween: String = "";
+  intervalo: any;
+  segundos: number = 0;
+
+  octubre31: number = 0;
+  rn: number = 0; //fecha ahora mismo
+  halloween: number = 0; //31 de octubre menos fecha actual
+
+
+  ngOnInit() {
+    this.contadorHalloween();
+  }
+
+  contadorHalloween() {
+    const octubre31 = new Date(new Date().getFullYear(),9,31,0,0,0);
+    this.intervalo = setInterval(() => {
+      this.rn = new Date().getTime();
+      this.halloween = octubre31.getTime() - this.rn;
+
+      if (this.halloween <= 0) {
+        return;
+      }
+        this.segundos = Math.floor(this.halloween/1000);
+        this.mensajeHalloween = `${this.segundos}`})
+    }
+  }
